fd3c73a2de7ecce7350baabf190efd73b34fb421
siyadlegend <103608474+siyadlegend@users.noreply.github.com>
2022-08-23 15:09:12 +1200

    Add files via upload

---
+++ lesson 1/basic_var_and_output.showdatatypee.py
@@ -0,0 +1,16 @@
+# S Hirey
+#Varibles
+
+# Declartion of varibales
+name ="Elephant"
+numLegs = 4
+height =3.8
+hasTail= True
+
+# Output to screen
+print(P'one of the big five animals in africa os called an',name,'.')
+print(:"An",name,"has",numLegs'legs.")
+print("Its height is approximatley",height,"metres.")
+print("And,yes, it's"hasTail,"It has a tail.")
+print("And yes, it's",hasTail,". It has a tail.")
+print("And yes, it's",hasTail,". It has a tail.")
