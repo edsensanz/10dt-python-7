d5c0956f634a9c22c81e35180021118e0b707d14
siyadlegend <103608474+siyadlegend@users.noreply.github.com>
2022-08-23 15:10:24 +1200

    Add files via upload

---
+++ lesson 1/basic_var_and_output.py
@@ -0,0 +1,15 @@
+# S Hirey
+#Varibles
+
+# Declartion of varibales
+name ="Elephant"
+numLegs = 4
+height =4.5
+hasTail= True
+
+# Output to screen
+print(name)
+print(numlegs)
+print(height)
+print(hasTail)
+
