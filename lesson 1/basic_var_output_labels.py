2828073de070b8ee7aa66adb9fcc93114d13988d
siyadlegend <103608474+siyadlegend@users.noreply.github.com>
2022-08-25 09:57:16 +1200

    Add files via upload

---
+++ lesson 1/basic_var_output_labels.py
@@ -0,0 +1,17 @@
+# S Hirey
+# Varibles, output, labels
+
+# Declaration of variables 
+from operator import truediv
+name = "elephant"
+numLegs = 4
+height = 4.5
+hasTail = True
+# Output of variables to screen
+print(name)
+print(numLegs)
+print(height)
+print(hasTail)
+
+# Output of variables with labels to screen
+print("The",name,"is one of the big five animals in Africa")
